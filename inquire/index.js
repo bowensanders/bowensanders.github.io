357bdd5e14abe49b0b35d585080fd6f06a37954e
Bowen Sanders <bowen@giveth.io>
2018-09-19 16:25:17 -0700

    thens to awaits, added comments

--- inquire/index.js
+++ inquire/index.js
@@ -1,34 +1,34 @@
+//
+// Simple query ETH address for milestones (bowensanders 9.19.18)
+//
+// URL to view data: https://feathers.beta.giveth.io/milestones/?$sort%5BupdatedAt%5D=-1&recipientAddress=0x...
 async function getResults(query) {
-  const res = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://feathers.beta.giveth.io/milestones/?$sort%5BupdatedAt%5D=-1&recipientAddress=${query}`
-  )
-    .then(result => {
-      //console.log(result);
-      return result.json();
+  try {
+    // get the data from feathers
+    const res = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://feathers.beta.giveth.io/milestones/?$sort%5BupdatedAt%5D=-1&recipientAddress=${query}`);
+    // convert results to json  
+    const data = await res.json();
+    // uncomment next console log for object display
+    // console.log(data);
+    console.log(`Below are the links to matching milestones for this address.`);
+    console.log(`------------------------------------------------------------`);
+    // format ETH values to 2 digits or 1 digit with space before decimal, 8 decimal places long
+    // then print each line to console
+    data.data.forEach(element => {
+      let eth = (element.maxAmount / 1000000000000000000)
+      let eth2 = "";
+      if (eth < 10) { 
+        eth2 = ` ${eth.toFixed(8)}`;
+      } else {
+        eth2 = eth.toFixed(8);
+      };
+      console.log(
+        `${eth2} ETH | https://beta.giveth.io/campaigns/${element.campaignId}/milestones/${element._id} | ${element.status}`);
     })
-    .then(data => {
-      console.log(`Below are the links to matching milestones for this address.`);
-      console.log(`------------------------------------------------------------`);
-      data.data.forEach(element => {
-          let eth = (element.maxAmount / 1000000000000000000)
-          let eth2 = "";
-          if (eth < 10) { 
-            eth2 = ` ${eth.toFixed(8)}`;
-          } else {
-            eth2 = eth.toFixed(8);
-          }
-        console.log(
-            `${eth2} ETH | https://beta.giveth.io/campaigns/${element.campaignId}/milestones/${element._id} | ${element.status}`
-          );
-        
-      });
-      
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  //console.log(res);
+  } catch(error) {
+    console.log(error);
+  }
 }
 
-//getResults("0xd00cc82a132f421bA6414D196BC830Db95e2e7Dd"); /
 getResults(prompt("[beta.giveth.io] OPEN CONSOLE (CMD-OPT-J) Check What Recipient Address?"));
