e982f886b32a423cd604468c284b4ddfc5ed9783
Bowen Sanders <bowen@giveth.io>
2018-09-18 18:45:14 -0700

    latest version.

--- inquire/index.js
+++ inquire/index.js
@@ -3,12 +3,12 @@ async function getResults(query) {
     `https://cors-anywhere.herokuapp.com/https://feathers.beta.giveth.io/milestones/?$sort%5BupdatedAt%5D=-1&recipientAddress=${query}`
   )
     .then(result => {
-      console.log(result);
+      //console.log(result);
       return result.json();
     })
     .then(data => {
-      console.log(data);
-      
+      console.log(`Below are the links to matching milestones for this address.`);
+      console.log(`------------------------------------------------------------`);
       data.data.forEach(element => {
           let eth = (element.maxAmount / 1000000000000000000)
         console.log(
@@ -25,4 +25,4 @@ async function getResults(query) {
 }
 
 //getResults("0xd00cc82a132f421bA6414D196BC830Db95e2e7Dd");
-getResults(prompt("[beta.giveth.io] Check What Recipient Address?"));
+getResults(prompt("[beta.giveth.io] OPEN CONSOLE (CMD-OPT-J) Check What Recipient Address?"));
