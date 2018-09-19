1dd9d4870bc06738930125488a999cc66f4848ab
Bowen Sanders <bowen@giveth.io>
2018-09-18 18:40:25 -0700

    Test

---
+++ inquire/index.js
@@ -0,0 +1,28 @@
+async function getResults(query) {
+  const res = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://feathers.beta.giveth.io/milestones/?$sort%5BupdatedAt%5D=-1&recipientAddress=${query}`
+  )
+    .then(result => {
+      console.log(result);
+      return result.json();
+    })
+    .then(data => {
+      console.log(data);
+      
+      data.data.forEach(element => {
+          let eth = (element.maxAmount / 1000000000000000000)
+        console.log(
+            `${eth} ETH https://beta.giveth.io/campaigns/${element.campaignId}/milestones/${element._id}`
+          );
+        
+      });
+      
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  //console.log(res);
+}
+
+//getResults("0xd00cc82a132f421bA6414D196BC830Db95e2e7Dd");
+getResults(prompt("[beta.giveth.io] Check What Recipient Address?"));
